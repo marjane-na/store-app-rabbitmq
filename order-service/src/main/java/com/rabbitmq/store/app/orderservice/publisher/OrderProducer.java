9139c95721ce361569c70f3cd530fbd122cf99e6
Marjane Namavar <marjane.namavar@sap.com>
2023-10-30 20:10:51 -0700

    Add base project

---
+++ order-service/src/main/java/com/rabbitmq/store/app/orderservice/publisher/OrderProducer.java
@@ -0,0 +1,35 @@
+package com.rabbitmq.store.app.orderservice.publisher;
+
+import com.rabbitmq.store.app.orderservice.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+
+    private Logger LOGGER = LoggerFactory.getLogger(OrderProducer.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.binding.routing.key}")
+    private String orderRoutingKey;
+
+    @Value("${rabbitmq.binding.email.routing.key}")
+    private String emailRoutingKey;
+
+    private RabbitTemplate rabbitTemplate;
+
+    public OrderProducer(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(OrderEvent orderEvent){
+        LOGGER.info(String.format("Order event sent to RabbitMQ: %s", orderEvent.toString()));
+        rabbitTemplate.convertAndSend(exchange, orderRoutingKey, orderEvent);
+        rabbitTemplate.convertAndSend(exchange, emailRoutingKey, orderEvent);
+    }
+}
